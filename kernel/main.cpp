ec3e2179421c0cb8a549894def27fcefb4fc2456
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2021-05-19 09:18:39 +0900

    add console class

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdint>
 #include <cstddef>
 #include <cstdio>
+#include "console.hpp"
 #include "font.hpp"
 #include "frame_buffer_config.hpp"
 #include "graphics.hpp"
@@ -12,8 +13,24 @@ void *operator new(size_t size, void *buf) {
 void operator delete(void *obj) noexcept {
 }
 
+char console_buf[sizeof(Console)];
+Console *console;
+
 char pixel_writer_buf[sizeof(RGBResv8BitPerColorPixelWriter)];
 PixelWriter *pixel_writer;
+
+int printk(const char *format, ...) {
+  va_list ap;
+  int result;
+  char s[1024];
+
+  va_start(ap, format);
+  result = vsprintf(s, format, ap);
+  va_end(ap);
+
+  console->PutString(s);
+  return result;
+}
 
 extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
   switch (frame_buffer_config.pixel_format) {
@@ -31,21 +48,11 @@ extern "C" void KernelMain(const FrameBufferConfig &frame_buffer_config) {
     }
   }
 
-  for (int x = 0; x < 200; ++x) {
-    for (int y = 0; y < 100; ++y) {
-      pixel_writer->Write(x, y, {0, 255, 0});
-    }
+  console = new(console_buf) Console(*pixel_writer, {255, 255, 255}, {0, 0, 0});
+
+  for (int i = 0; i < 27; ++i) {
+    printk("printk: %d\n", i);
   }
-
-  for (int c = 0x21; c <= 0x7e; ++c) {
-    WriteAscii(*pixel_writer, (c - 0x21) * 8, 50, c, {0, 0, 0});
-  }
-
-  WriteString(*pixel_writer, 0, 66, "Hello World!!", {0, 0, 255});
-
-  char buf[128];
-  sprintf(buf, "1 + 2 = %d", 1 + 2);
-  WriteString(*pixel_writer, 0, 82, buf, {255, 0, 0});
 
   while (1) __asm__("hlt");
 }
