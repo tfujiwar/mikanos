b439507763a9b749d4b1080fa4bbf6b2accf3626
Taketoshi Fujiwara <taketoshi.fujiwara@gmail.com>
2021-06-17 22:37:41 +0900

    add IdleTask

--- kernel/task.cpp
+++ kernel/task.cpp
@@ -11,6 +11,10 @@ namespace {
     auto it = std::remove(c.begin(), c.end(), value);
     c.erase(it, c.end());
   }
+
+  void TaskIdle(uint64_t task_id, int64_t data) {
+    while (true) __asm__("hlt");
+  }
 } // namespace
 
 Task::Task(uint64_t id) : id_{id}, msgs_{} {}
@@ -71,6 +75,9 @@ std::optional<Message> Task::ReceiveMessage() {
 TaskManager::TaskManager() {
   Task &task = NewTask().SetLevel(current_level_).SetRunning(true);
   running_[current_level_].push_back(&task);
+
+  Task &idle = NewTask().InitContext(TaskIdle, 0).SetLevel(0).SetRunning(true);
+  running_[0].push_back(&idle);
 }
 
 Task &TaskManager::NewTask() {
